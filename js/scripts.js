743cf91790c73215c09e412dba750c5f28f37165
CCInCharge <charles.l.chen.clc@gmail.com>
2016-12-26 16:39:00 -0800

    Added code to display search results

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,21 @@ $.ajax({
 
 function showData(data) {
   console.log(data);
-  results = data.query.search;
+  search_results = data.query.search;
+  $("#results").empty();
+  for (i = 0; i < search_results.length; i++) {
+    $("#results").append("<p>" + search_results[i].title + "</p>");
+  }
 }
 
 $(document).ready(function(){
   $(".search-button").click(function(){
     searchData($(".search-form").val());
+  });
+
+  $(".search-form").keyup(function(event){
+    if(event.keyCode == 13){
+        $(".search-button").click();
+    }
   });
 });
